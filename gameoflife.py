53ff915d37dbc8b936055b51068e3dd62edabe1b
BEAFbetween <benfleming1@live.co.uk>
2022-01-09 17:40:03 +0000

    commit

--- gameoflife.py
+++ gameoflife.py
@@ -1,3 +1,7 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import math
+
+def init_array(self,n,vals):
+    return np.random.choice
