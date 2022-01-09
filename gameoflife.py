c9481954751327f0d0ed47d1b533d8931d69ad95
BEAFbetween <benfleming1@live.co.uk>
2022-01-09 22:53:53 +0000

    initial array definition

--- gameoflife.py
+++ gameoflife.py
@@ -3,5 +3,5 @@ import matplotlib.animation as animation
 import numpy as np
 import math
 
-def init_array(self,n,vals):
-    return np.random.choice
+def init_array(n, vals):
+    return np.random.choice(vals, n*n, p=[0.2,0.8]).reshape(n, n)
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import math
+import gameoflife
+
+LIVE = 1
+DEAD = 0
+vals = [LIVE,DEAD]
+init = gameoflife.init_array(6, vals)
