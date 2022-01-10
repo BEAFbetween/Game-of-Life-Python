8210f65e179052488b1738e208ddeac4b7e2761b
BEAFbetween <benfleming1@live.co.uk>
2022-01-10 01:16:23 +0000

    commenting

--- gameoflife.py
+++ gameoflife.py
@@ -6,11 +6,11 @@ import math
 from main import DEAD, LIVE
 
 def init_array(n, vals):
-    return np.random.choice(vals, n*n, p=[0.2,0.8]).reshape(n, n)
+    return np.random.choice(vals, n*n, p=[0.2,0.8]).reshape(n, n) # creates a square array of size n with probabilities p associated with either LIVE or DEAD
 
-def check_cell(array, i, j, n, newGrid):
+def check_cell(array, i, j, n, newGrid): # totaling the number of LIVE cells neighbouring the chosen cell
     total = array[(i-1)%n,(j-1)%n] + array[(i-1)%n, j] + array[(i-1)%n, (j+1)%n] + array[i, (j+1)%n] + array[(i+1)%n, (j+1)%n] + array[(i+1)%n, j] + array[(i+1)%n, (j-1)%n] + array[i, (j-1)%n]
-    if array[i,j] == LIVE:
+    if array[i,j] == LIVE: # applying Conway's rules
         if (total < 2) or (total > 3):
             newGrid[i,j] = DEAD
     else:
--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import gameoflife
 LIVE = 1
 DEAD = 0
 vals = [LIVE,DEAD]
-init = gameoflife.init_array(6, vals)
+init = gameoflife.init_array(6, vals) # create initial conditions in an array
+newGrid = init.copy() # create a copy of that array that will become the grid after one time interval
 
 fig, ax = plt.subplots()
-img = plt.imshow(init,interpolation='nearest')
+img = plt.imshow(init, interpolation='nearest')
 
 plt.show()
